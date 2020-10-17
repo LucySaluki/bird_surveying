187466fc68023189f3b7439d2584a261c1939217
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-17 15:01:56 +0100

    initial commit

---
+++ bird_surveying/src/main/java/com/codeclan/example/bird_surveying/BirdSurveyingApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bird_surveying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdSurveyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdSurveyingApplication.class, args);
+	}
+
+}
---
+++ bird_surveying/src/test/java/com/codeclan/example/bird_surveying/BirdSurveyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.bird_surveying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdSurveyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
